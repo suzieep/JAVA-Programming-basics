35a96b6e8ca3cf69b7ee74a5338db004bc04a19a
Suzie Lee <wnsuwls0707@gmail.com>
2020-09-28 19:16:56 +0900

    0908 class

---
+++ assignment2/src/assignment2/q1.java
@@ -0,0 +1,17 @@
+package assignment2;
+
+public class q1 {
+
+	public static void main(String[] args) {
+		
+		int[][] matrix1= new int[][] {
+			{1,1,1,1}{1,1,1,1}
+			};
+		int matrix2[][] = new int[][] {
+			{1,2,3}{5,6,7}{1,1,1}{1,1,1}
+		};
+
+	}
+
+}
+
---
+++ example/src/example/example0928.java
@@ -0,0 +1,10 @@
+package example;
+
+public class example0928 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
