97f2a93794d7fe9da6b4ab7deaa98c242e4d12f8
Suzie Lee <wnsuwls0707@gmail.com>
2020-10-13 18:13:40 +0900

    chores

---
+++ JAVA-program/hw3/FileInOut_1.java
@@ -0,0 +1,10 @@
+//1.	(40점) 파일 first.txt와 second.txt는 각각 숫자를 10개씩 저장하고 있다. 
+//저장된 숫자는 스페이스로 구분되어 저장된다. 
+//이떄 first.txt와 second.txt의 숫자를 읽어서 두 파일 모두에 공통적으로 저장되어 있는 숫자들을 third.txt에 저장하는 프로그램을 작성하여라. 
+
+package hw3;
+
+
+public class FileInOut_1{
+    
+}
