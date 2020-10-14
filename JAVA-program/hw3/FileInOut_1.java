f009be6b9b6594bfbc5d1a69a9dc3e391f32f443
Suzie Lee <wnsuwls0707@gmail.com>
2020-10-14 18:19:53 +0900

    chores

--- JAVA-program/hw3/FileInOut_1.java
+++ JAVA-program/hw3/FileInOut_1.java
@@ -3,8 +3,28 @@
 //이떄 first.txt와 second.txt의 숫자를 읽어서 두 파일 모두에 공통적으로 저장되어 있는 숫자들을 third.txt에 저장하는 프로그램을 작성하여라. 
 
 package hw3;
-
-
+import java.io.*;
 public class FileInOut_1{
-    
+    public static void main(String args[]) throws IOException {
+        FileInputStream in1 = null;
+      //  FileInputStream in2 = null;
+        FileOutputStream out = null;
+        try {
+        in1 = new FileInputStream("first.txt");
+     //   in2 = new FileInputStream("second.txt");
+        out = new FileOutputStream("third.txt");
+        int c;
+        while ((c = in1.read()) != -1) {
+        out.write(c);
+        }
+        }finally {
+        if (in1 != null) {
+        in1.close();
+        }
+        if (out != null) {
+        out.close();
+
+        }
+        }
+        }
 }
