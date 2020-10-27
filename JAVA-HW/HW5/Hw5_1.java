bea693a763fbcb0a52d868a6c7f6a779b445ec00
Suzie Lee <wnsuwls0707@gmail.com>
2020-10-28 03:30:38 +0900

    hw5 started

---
+++ JAVA-HW/HW5/Hw5_1.java
@@ -0,0 +1,8 @@
+package HW5;
+
+public class Hw5_1 {
+    public static void main(String[] args){
+        
+    }
+    
+}
---
+++ JAVA-HW/HW5/Hw5_2.java
@@ -0,0 +1,9 @@
+package HW5;
+
+public class Hw5_2 {
+
+    public static void main(String[] args){
+        
+    }
+    
+}
